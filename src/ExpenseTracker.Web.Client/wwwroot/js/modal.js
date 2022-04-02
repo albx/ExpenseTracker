0d32cc034b7f3b8fc81311018975d1e3d5f8e040
Alberto Mori <albi.mo87@hotmail.it>
2022-04-02 18:10:25 +0200

    added modal on delete

---
+++ src/ExpenseTracker.Web.Client/wwwroot/js/modal.js
@@ -0,0 +1,10 @@
+﻿var modal;
+
+window.showModal = (modalId) => {
+    modal = new bootstrap.Modal(document.getElementById(modalId), {});
+    modal.show();
+};
+
+window.hideModal = (modalId) => {
+    modal.hide();
+};
